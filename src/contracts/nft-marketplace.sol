bdd44c37a1807004289a49ec59cc6a5c11e51909
xd-Abi <abinayansureskumar@gmail.com>
2023-05-28 00:47:53 +0200

    Added favicon and compiled new solidity code

--- src/contracts/nft-marketplace.sol
+++ src/contracts/nft-marketplace.sol
@@ -4,12 +4,12 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract NFTMarketplace {
 
-    mapping (address => bytes32) nfts;
+    mapping (address => string) nfts;
     uint counter = 1;
 
-    function mint() public returns (bytes32) {
-        nfts[msg.sender] = bytes32(abi.encode("http://fyndit.io/nfts", counter, ".png"));
+    function mint() public returns (address) {
+        nfts[msg.sender] = string(abi.encode("http://fyndit.io/nfts", counter, ".png"));
         counter++;
-        return nfts[msg.sender];
+        return msg.sender;
     }
 }
